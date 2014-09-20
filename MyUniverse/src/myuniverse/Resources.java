f0e9f9a28fbfe0bf59fb47b16d40417d71993f27
Lawrence Moore <lsmoore938@gmail.com>
2014-09-20 17:23:03 -0400

    Lawrence - implemented solar system and planet class

---
+++ MyUniverse/src/myuniverse/Resources.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myuniverse;
+
+/**
+ *
+ * @author lsmoore
+ */
+public enum Resources {
+    NOSPECIALRESOURCES,
+    MINERALRICH,
+    MINERALPOOR,
+    DESERT,
+    LOTSOFWATER,
+    RICHSOIL,
+    POORSOIL,
+    RICHFAUNA,
+    LIFELESS,
+    WEIRDMUSHROOMS,
+    LOTSOFHERBS,
+    ARTISTIC,
+    WARLIKE,
+}
